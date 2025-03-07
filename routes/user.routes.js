fbc12e248ed9d94a1fc5a9465dbb02af33d6ef09
Kamran Shakib <kamranshakib371@gmail.com>
2025-03-07 09:12:19 -0800

    create routes file for follwind MVC

---
+++ routes/user.routes.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.models')
+
+
+// hash password 
+async function cryptPassword(password){
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt)
+   return hash
+     
+}
+
+// register users
+router.post('/register', async (req,res)=>{
+   req.body.password = await cryptPassword(req.body.password)
+   const newUser =  new userModel(req.body);
+    await newUser.save()
+   res.send(newUser)
+})
+  
+// login users
+router.get('/login',async(req,res)=>{
+ const {email, phone , password} = req.body
+ const foundUser = await userModel.findOne({$or:[{email}, {phone}]})
+  if(foundUser){
+  const comparePassword = await bcrypt.compare(password , foundUser.password)
+  res.send(comparePassword)
+ }
+ 
+})
+
+router.post('/login',(req,res)=>{
+
+})
+
+module.exports = router
+
