f7fa41824e9418944cfd661add6dd8be11e0e186
Kamran Shakib <kamranshakib371@gmail.com>
2025-03-10 11:11:20 -0700

    change code to MVC form

---
+++ controllers/user.controller.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const userModel = require("../models/user.models");
+
+
+// register
+const register = async (req, res) => {
+  const { email, phone, name, password } = req.body;
+
+  const foundUser = await userModel.findOne({ $or: [{ email }, { phone }] });
+  if (foundUser) {
+    return res.json("کاربر از قبل وجود دارد ! ");
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      const newUser = await userModel.create({
+        email,
+        name,
+        phone,
+        password: hash,
+      });
+    });
+    let token = jwt.sign(
+      { email: email, userId: userModel._id },
+      process.env.PRIMARY_KEY
+    );
+    res.cookie("token", token);
+    res.json({ msg: "Your register went succesfully" });
+  });
+};
+
+
+// login
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const foundUser = await userModel.findOne({ $or: [{ email }] });
+  if (foundUser) {
+    const PasswordisCorrect = await bcrypt.compare(
+      password,
+      foundUser.password
+    );
+    if (PasswordisCorrect) {
+      let token = jwt.sign(
+        { _id: foundUser._id, email: foundUser.email },
+        process.env.PRIMARY_KEY
+      );
+      res.cookie("token", token);
+      res.json({ msg: "your login went succsefuly" });
+    } else res.json({ msg: "your password is not match" });
+  } else {
+    res.render("register");
+  }
+}; 
+
+// logout
+const logout = (req, res) => {
+  res.cookie("token", "");
+  res.json({ msg: "token went remove" });
+};
+
+
+// getLogin
+const getLogin = (req, res) => {
+  res.render("login");
+};
+
+
+// profile
+const profile = (req, res) => {
+  res.json(req.user);
+};
+
+
+module.exports = {register,login,logout,getLogin,profile};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,97 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require("bcrypt");
+const express = require("express");
+const router = express.Router();
 const jwt = require('jsonwebtoken')
-require('dotenv').config()
-const userModel = require('../models/user.models')
-
-async function cryptPassword(password) {
-  const salt = await bcrypt.genSalt(10);
-  const hash = await bcrypt.hash(password, salt);
-  return hash;
-}
-    
-// profile
-router.get('/profile',inLoggedin, (req,res)=>{
-  res.json(req.user)
-
-})
-// register users
-        
-router.post('/register',async  (req,res)=>{
-   const {email, phone , name , password} = req.body
-   
-   const foundUser =  await userModel.findOne({$or:[{email}, {phone}]})
-   if(foundUser){
-      return res.json('کاربر از قبل وجود دارد ! ')
-   }
-    bcrypt.genSalt(10,(err,salt)=>{
-      bcrypt.hash(password,salt, async (err,hash)=>{
-        const newUser = await userModel.create({
-          email ,
-          name  ,
-          phone ,
-          password: hash
-        })
-
-      })
-      let token = jwt.sign(
-        { email: email, userId: userModel._id },
-        process.env.PRIMARY_KEY
-      );
-      res.cookie("token",token) 
-      res.json({msg: 'Your register went succesfully'})
-      
-    })
-
-})   
-// hash password 
-
-    
-// login users
-router.post('/login',async(req,res)=>{
- const {email , password} = req.body
- const foundUser = await userModel.findOne({$or:[{email}]})
-  if(foundUser){
-  const PasswordisCorrect = await bcrypt.compare(password , foundUser.password)
-   if (PasswordisCorrect){ 
-    let token = jwt.sign(
-      { _id: foundUser._id, email: foundUser.email },
-      process.env.PRIMARY_KEY
-    );
-    res.cookie("token", token);
-    res.json({msg: "your login went succsefuly"})
-      
-  } else res.json({ msg: "your password is not match" });
-      
- }else{
-   res.render('register')
- }
- 
-})
-
-router.get('/logout',(req,res)=>{
-  res.cookie("token","")
-  res.json({msg: 'token went remove'})
-
-})
+const userModel = require("../models/user.models");
+const {register,login,logout,getLogin,profile} = require('../controllers/user.controller') 
 
 
-function inLoggedin(req,res,next){
-  if(req.cookies.token ===""){
-    res.json({msg: "you shouls loggin first"})
-  }else{
-    let data = jwt.verify(req.cookies.token, process.env.PRIMARY_KEY)
+
+router.get("/profile", inLoggedin, profile);
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/logout", logout);
+
+router.get("/login", getLogin);
+
+function inLoggedin(req, res, next) {
+  if (req.cookies.token === "") {
+    res.json({ msg: "you shouls loggin first" });
+  } else {
+    let data = jwt.verify(req.cookies.token, process.env.PRIMARY_KEY);
     req.user = data;
   }
-  next()
-
-
+  next();
 }
-router.get('/login',(req,res)=>{
-  res.render('login')
-})
 
-
-module.exports = router
-
+module.exports = router;
